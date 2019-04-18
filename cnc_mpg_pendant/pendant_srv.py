f9276a841ffc9b7009b8ee2cba1dc85d302265ca
Pettersson Johan <johan.pettersson@volvo.com>
2019-04-18 08:20:28 +0200

    cleaning up, parsing xml and adding HAL stub

--- cnc_mpg_pendant/pendant_srv.py
+++ cnc_mpg_pendant/pendant_srv.py
@@ -14,128 +14,77 @@ class CMD:
       self.name = name
       self.val = val
       
-## handler for G-words. only G0 and G1 currently supported
-def handle_g(str):
-   retVal = ''
-
-   if (str == 'G0' or str == 'G00'): 
-      retVal = 'G0'
-      
-   if (str == 'G1' or str == 'G01'):
-      retVal = 'G1'
-      
-   if (str == 'G28'):
-      retVal = 'G28'
-      
-   if (str == 'G92'):
-      retVal = 'G92'
-      
-   if (str == 'G52'):
-      retVal = 'G52'
-
-   return retVal
-
-## handler for X-words, converts from mm to 10um unit
-def handle_x(in_str):
-   tmp_f = float(in_str[1:len(in_str)])
-   tmp_i = int(round(tmp_f*100))
-   ret_str = 'X' + str(tmp_i)
-   return ret_str
-
-## handler for Y-words
-def handle_y(in_str):
-   tmp_f = float(in_str[1:len(in_str)])
-   tmp_i = int(round(tmp_f*100))
-   ret_str = 'Y' + str(tmp_i)
-   return ret_str
+class hal_pin:
+   """stub sub class to hal-class"""
    
-## handler for Z-words
-def handle_z(in_str):
-   tmp_f = float(in_str[1:len(in_str)])
-   tmp_i = int(round(tmp_f*100))
-   ret_str = 'Z' + str(tmp_i)
-   return ret_str
-   
-## handler for F-words
-def handle_f(in_str):
-   try:
-      ret_str = 'F' + str(int(in_str[1:len(in_str)]))
-   except ValueError:
-      ret_str = ''
+   def __init__(self, name, type):
+      self.name = name
+      self.type = type
 
-   return ret_str
+class hal:
+   """stub class linuxCNC hal-api"""
+   HAL_BIT = 'HAL_BIT'
+   HAL_S32 = 'HAL_S32'
+   HAL_U32 = 'HAL_U32'
+   HAL_OUT = 'HAL_OUT'
    
-## handler for M-words
-def handle_m(str):
-   try:
-      ret_str = 'M' + str(int(in_str[1:len(in_str)]))
-   except ValueError:
-      ret_str = ''
-
-   return ret_str
+   pin_list = []
+   ready_flag = 0;
+   def component(self, name):
+      self.name = name
+   def newpin(self, pin_name, pin_type, dir):
+      self.pin_list.append(hal_pin(pin_name.strip('"'), pin_type))
+   def ready():
+      ready = 1
+   def __getitem__(self, name):
+      for pin in self.pin_list:
+         if pin.name == name:
+            return pin
+   
    
 ### parse_line() #############################################
 def parse_cmd(str):
   
-  words = str.split()
+  words = str.split('_')
   send_cmd = []             #temp list to store commands to send
   
-  for word in words:       #go through each word in splitted line
-    for cmd in cmds:       #compare word to each supported command
-      if word[0] == cmd.function_code:   #is the word any of the supported commands in cmds
-         tmp_word = cmd.callback(word)  #call command-specific handler, i.e parse command
-         if tmp_word != '':
-            send_cmd.append(tmp_word) #add command to send-message, send_cmd is a list of strings
-
-  return ''.join(send_cmd) #join will append the list of strings to one string
+#  for word in words:       #go through each word in splitted line
+#    for cmd in cmds:       #compare word to each supported command
+#      if word[0] == cmd.function_code:   #is the word any of the supported commands in cmds
+#         tmp_word = cmd.callback(word)  #call command-specific handler, i.e parse command
+#         if tmp_word != '':
+#            send_cmd.append(tmp_word) #add command to send-message, send_cmd is a list of strings
+#
+#  return ''.join(send_cmd) #join will append the list of strings to one string
   
-  
-## wait for character ch sent from client ################################## 
-def waitForToken(ch):
-   if debug == 0:
-      b = ser.read(1) #blocking
-      print 'rec: ' + b
-      if ser.in_waiting:
-         while b != ch:
-            print b
-            b = ser.read(1)
-   
-   if debug == '1':
-      s = raw_input('\tpress any key:') 
  
 ### start of main script #############################################
-
-#these are the accepted commands everything else will be thrown away
-cmds = [CMD('G', handle_g), CMD('X', handle_x), CMD('Y', handle_y), CMD('Z', handle_z), CMD('F', handle_f), ]
 
 in_file = ''
 debug = 1
 port = '/dev/ttyS2'
+in_file = 'config/axis_mm.ini'
 
-try:
-  opts, args = getopt.getopt(sys.argv[1:], "h", ["input=", "debug=", "port="])
-except getopt.GetoptError as err:
-  # print help information and exit:
-  print(err) # will print something like "option -a not recognized"
-  sys.exit(2)
+f_in = open(in_file, 'r')
 
-for o, a in opts:
-  if o == "-h":
-    print 'usage pendant_srv.py --input=<in.xml> --debug=[012] --port=<serial port>'
-    sys.exit()
-  elif o == "--input":
-    in_file = a
-  elif o == "--debug":
-    debug = a
-  elif o == "--port":
-    port = a
-  else:
-    print o
-    assert False, "unhandled option"
-
-if in_file == '':
-   print 'usage pendant_srv.py --input=<in.xml> --debug=[012] --port=<serial port>'
-   sys.exit()
+# parse for .xml-file
+for line in f_in:
+   if 'ARPY_MPG' in line:     
+      # remove comment if any
+      i = line.find('#')
+      if i >= 0:
+         line = line[:i]
+      
+      # get the file name
+      i = line.find('=')
+      if i >= 0:
+         line = line[i+1:]
+         
+      # remove blank-spaces
+      line = line.strip(' ')
+      
+      # remove line ending
+      xml_file = line.strip('\n')
 
 # open serial port
 # list available ports with 'python -m serial.tools.list_ports'
@@ -145,11 +94,11 @@ ser.baudrate = 9600
 ser.parity = 'N'
 ser.bytesize = 8
 ser.stopbits = 1
-ser.xonxoff = False       #disable software flow control
-#ser.rtscts = False       #disable hardware (RTS/CTS) flow control
-#ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
-#ser.writeTimeout = 2     #timeout for write
-#ser.timeout = 3
+ser.xonxoff = False       # disable software flow control
+#ser.rtscts = False       # disable hardware (RTS/CTS) flow control
+#ser.dsrdtr = False       # disable hardware (DSR/DTR) flow control
+#ser.writeTimeout = 2     # timeout for write
+ser.timeout = 1           # 1 sec timeout
 
 if debug == 0:
    try:
@@ -162,7 +111,10 @@ if debug == 0:
       sys.exit(1)
 
 
-tree = ET.parse(in_file)
+h = hal()
+h.component("mpg")
+
+tree = ET.parse(xml_file)
 root = tree.getroot()
       
 for halpin in root.iter('halpin'):
@@ -170,5 +122,18 @@ for halpin in root.iter('halpin'):
    event = halpin.find('event')
    
    if type is not None and event is not None:
-      print halpin.text, type.text, event.text
+      #print 'l'+halpin.text + 'r', type.text, event.text
+      h.newpin(halpin.text, type.text, hal.HAL_OUT)
+
+for pin in h.pin_list:
+   #print pin.name, pin.type
+   print h[pin.name].name
+
+ #ser.write(''.join(str).encode('utf-8'))
+ #ser.write('\n')
+ #serial expects a byte-array and not a string
+if debug == 0:
+   if ser.in_waiting:
+      b = ser.read_until() #blocks until '\n' received or timeout
+      parse_cmd(b.Decode('utf-8'))
       
