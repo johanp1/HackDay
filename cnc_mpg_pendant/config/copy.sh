c94624d221bb05d9cc92d4f454c83266e29e668c
johan <johan@localhost.localdomain>
2019-05-22 22:14:23 +0200

    adding selector and dmux4 to mpg2.hal

--- cnc_mpg_pendant/config/copy.sh
+++ cnc_mpg_pendant/config/copy.sh
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 #!/bin/sh
-sudo cp mpg.xml ~/linuxcnc/configs/sim.axis/mpg.xml
-sudo cp mpg.hal ~/linuxcnc/configs/sim.axis/mpg.hal
-=======
-#!/bin/bash
-
-cp mpg.xml ~/linuxcnc/configs/sim.axis/.
-cp mpg.hal ~/linuxcnc/configs/sim.axis/.
-
->>>>>>> a93b1fbad1ccfda0c8e55139ac4211d0720f2b43
-#cp axis_mm.ini ~/linuxcnc/configs/sim.axis/.
+cp mpg.xml ~/linuxcnc/configs/sim.axis/mpg.xml
+cp mpg.hal ~/linuxcnc/configs/sim.axis/mpg.hal
+cp mpg2.hal ~/linuxcnc/configs/sim.axis/mpg2.hal
