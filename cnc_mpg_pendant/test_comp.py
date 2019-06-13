a47af99386343113cca3a50f06de2306f5a2249d
Pettersson Johan <johan.pettersson@volvo.com>
2019-06-13 13:38:20 +0200

    Update test_comp.py

--- cnc_mpg_pendant/test_comp.py
+++ cnc_mpg_pendant/test_comp.py
@@ -10,5 +10,15 @@ class TestComp(unittest.TestCase):
       
    def test_init(self):
       self.assertTrue(len(self.c.evToHALPin) == 0)
-   
-   
+ 
+   def test_addOnePin(self):
+      self.c.addPin('apa', 'apa_pin', 'u32')
+      self.assertTrue(len(self.c.evToHALPin) == 1)
+      self.assertTrue(self.c.evToHALPin['apa'].name == 'apa_pin')
+      self.assertTrue(self.c.evToHALPin['apa'].val == 0)
+      self.assertTrue(self.c.evToHALPin['apa'].type == 'u32')
+
+ 
+   def test_updatePin(self):
+      e = comms.event('apa', '12')
+      
