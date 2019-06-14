34a3a80adf9044a55ad9773589447931a430bfaa
johan <johan@localhost.localdomain>
2019-06-14 20:58:14 +0200

    config update

--- cnc_mpg_pendant/config/copy.sh
+++ cnc_mpg_pendant/config/copy.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 cp mpg.xml ~/linuxcnc/configs/sim.axis/mpg.xml
 cp mpg.hal ~/linuxcnc/configs/sim.axis/mpg.hal
-cp vcp-mpg.xml ~/linuxcnc/configs/sim.axis/vcp-mpg.xml
-cp vcp-mpg.hal ~/linuxcnc/configs/sim.axis/vcp-mpg.hal
+cp axis_mm.ini ~/linuxcnc/configs/sim.axis/.
