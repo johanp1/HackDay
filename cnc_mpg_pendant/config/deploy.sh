223172c69ad57b3256028ec1fb08fcf094f34897
Pettersson Johan <johan.pettersson@volvo.com>
2019-09-26 06:00:21 +0100

    same as prev.

---
+++ cnc_mpg_pendant/config/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cp test.hal ~/linuxcnc/configs/test/.
+cp test.ini ~/linuxcnc/configs/test/.
+
+cp ../serial_mpg.py ~/linuxcnc/configs/test/.
+cp ../comms.py ~/linuxcnc/configs/test/.
+
+cp mpg.xml ~/linuxcnc/configs/test/.
+cp mpg.hal ~/linuxcnc/configs/test/.
+
+#cp axis_mm.ini ~/linuxcnc/configs/sim.axis/.
+cp switch_proxy.hal ~/linuxcnc/configs/test/.
+cp switch_proxy.ini ~/linuxcnc/configs/test/.
