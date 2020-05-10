3754ae339e437bdb65334d54f6556fd9d116c9e3
johan.p1sson@gmail.com <johan.p1sson@gmail.com>
2020-05-10 17:06:08 +0200

    first shot

--- extruderTempCtrl/hal_exruder_temp_ctrl.py
+++ extruderTempCtrl/hal_exruder_temp_ctrl.py
@@ -18,21 +18,31 @@ class HalFacade:
    def __init__(self, name):
       self.hal = hal.component(name)  # instanciate the HAL-component
       self.hal.newpin("ref-temp", hal.HAL_U32, hal.HAL_IN)
+      self.hal.newpin("wait-temp", hal.HAL_U32, hal.HAL_IN)
       self.hal.newpin("curr-temp", hal.HAL_U32, hal.HAL_OUT)
+      self.hal.newpin("temp-ok", hal.HAL_BIT, hal.HAL_OUT)
+          
       self.hal.ready()
  
+   def setReady(self): 
+      self.hal.ready()
+
    def readPin(self, pin):
       return self.hal[pin]
-
-   def updateHAL(self, currTemp):
+         
+   def updateHAL(self):
       """ write internal wrapper pin values to LinuxCNC HAL """
-      self.hal['curr-temp'] = currTemp
+      self.hal['curr-temp'] = self.currTemp
+      self.hal['temp-ok'] = self.tempOk
 
-class MessageHandler:
-   def __init__(self):
+
+class TempControllerFacade:
+   def __init__(self, port):
+      self.tempController = comms.instrument(port, self.msgHandler) #serial adaptor
+
       self.currTemp = 100
 
-   def handler(self, m):
+   def msgHandler(self, m):
       if (m.msg == 'mv'):
          self.currTemp = int(m.val)
 
@@ -59,15 +69,18 @@ def main():
          assert False, "unhandled option"
 
    h = HalFacade(name)
-   hdlr = MessageHandler()
-   tempController = comms.instrument(port, hdlr.handler) #serial adaptor
+   tc = TempControllerFacade(port)
+   prevRefTemp = h.readPin('ref-temp')
 
    try:
          while 1:
             refTemp = h.readPin('ref-temp')
-            tempController.writeMessage(comms.Message('sp' , str(refTemp)))
+            if refTemp != prevRefTemp:
+                tempController.writeMessage(comms.Message('sp' , str(refTemp)))
+                prevRefTemp = refTemp
+                
             tempController.readMessages() #update current temp
-            h.updateHAL(hdlr.currTemp)
+            h.updateHAL()
             
             time.sleep(1)
 
