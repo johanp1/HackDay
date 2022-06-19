47237bef49120c40fdc8dbaff9923f5f83d76a1a
johan <johan.p1sson@gmail.com>
2022-06-19 18:54:03 +0200

    added lcd_writer

--- serialEventHdlr/EventProxy/src/lcd_writer.h
+++ serialEventHdlr/EventProxy/src/lcd_writer.h
@@ -4,42 +4,165 @@
 #include <Arduino.h>
 #include "event_parser.h"
 
-template <class Lcd>
-class LcdWriter : public CommandHandler
+using Position = float;
+using Speed = int;
+using Row = byte;
+enum Axis {axis_x, axis_y};
+
+class Observer
 {
- public:
-   LcdWriter(Lcd& lcd_, const String& cmd) : myLcd(lcd_), CommandHandler{cmd} {};
+   public:
+   Observer(){};
+   virtual void Update(){};
+};
 
-   void operator()()
+class Model
+{
+   public:
+   Model(){x_pos_ = 0.0f, y_pos_ = 0.0f, spindle_speed_ = 0; active_axis_ = axis_x;};
+
+   void Attach(Observer* o){registry = o;};
+   void Detach(Observer* o)
    {
-      myLcd.clear();
-      myLcd.home();
-      myLcd.setCursor(0, 0);
-      myLcd.print("*** first line.");
+      if (registry == o)
+      {
+         registry = nullptr;
+      }
    };
 
-   void operator()(String& _parsedData)
-   {
-      myLcd.clear();
-      myLcd.home();
-      myLcd.setCursor(0, 0);
-      //myLcd.print("*** first line.");
-      myLcd.print(_parsedData);
-   };
+   void SetX(Position p){x_pos_ = p; Notify();};
+   void SetY(Position p){y_pos_ = p; Notify();};
+   void SetSpindleSpeed(Speed s){spindle_speed_ = s; Notify();};
+   void SetActiveAxis(Axis a){active_axis_ = a;Notify();};
+
+   Position GetX(){return x_pos_;};
+   Position GetY(){return y_pos_;};
+   Speed GetSpindleSpeed(){return spindle_speed_;};
+   Axis GetActiveAxis(){return active_axis_;};
+
+   protected:
+   virtual void Notify(){registry->Update();};
 
    private:
-   Lcd& myLcd;
+   Position x_pos_;
+   Position y_pos_;
+   Speed spindle_speed_;
+   Axis active_axis_;
+   Observer* registry;
 };
 
-/*
 template <class Lcd>
-class LcdWriterChild : public LcdWriter {
+class LcdView : public Observer
+{
    public:
-   //UpdateXLcdWriter(Lcd& lcd_) : LcdWriter(lcd_, String("x")) {}; 
+   LcdView(Lcd& lcd, Model& model) : myLcd(lcd), myModel(model) {myModel.Attach(this);};
+   virtual ~LcdView(){myModel.Detach(this);};
+   virtual void Update(){this->Draw();};
+   virtual void Draw();
 
-   void operator()(String& _parsedData)
-   {};
+   protected:
+   Lcd& myLcd;
+   Model& myModel;
 };
-*/
+
+template <class Lcd>
+void LcdView<Lcd>::Draw()
+{
+   myLcd.clear();
+   myLcd.setCursor(0, 0);
+   myLcd.print(String("hej"));
+};
+
+template <class Lcd>
+class AxisView : public LcdView<Lcd> 
+{
+   public:
+   AxisView(Lcd& lcd, Model& model) : LcdView<Lcd>(lcd, model){};
+
+   void Draw() override;
+};
+
+template <class Lcd>
+void AxisView<Lcd>::Draw()
+{
+   String row1 = String(" x: ");
+   String row2 = String(" y: ");
+
+   row1.concat(this->myModel.GetX());
+   row2.concat(this->myModel.GetY());
+   
+   // indicate the jogged/active axis with a  '*'
+   row1.setCharAt(0, this->myModel.GetActiveAxis() == axis_x ? '*' : ' ');
+   row2.setCharAt(0, this->myModel.GetActiveAxis() == axis_y ? '*' : ' ');
+
+   this->myLcd.setCursor(0, 0);
+   this->myLcd.print(row1);
+   this->myLcd.setCursor(0, 1);
+   this->myLcd.print(row2);
+};
+
+template <class Lcd>
+class SpindleView : public LcdView<Lcd> 
+{
+   public:
+   SpindleView(Lcd& lcd, Model& model) : LcdView<Lcd>(lcd, model){};
+
+   void Draw() override;
+};
+
+template <class Lcd>
+void SpindleView<Lcd>::Draw()
+{
+   String row1 = String(" spindle speed:");
+   String row2 = String(" ");
+
+   row2.concat(this->myModel.GetSpindleSpeed());
+   row2.concat(String(" rpm"));
+
+   this->myLcd.setCursor(0, 0);
+   this->myLcd.print(row1);
+   this->myLcd.setCursor(0, 1);
+   this->myLcd.print(row2);
+};
+
+template <class Lcd>
+class LcdWriter
+{
+public:
+   LcdWriter(Lcd& lcd /*, uint8_t cols, uint8_t rows*/)  : myLcd(lcd)
+   {
+      LcdView<Lcd>* myView = new AxisView<Lcd>(myLcd, myModel);
+
+      myLcd.begin(16, 2);
+      myLcd.setBacklight(200);
+   };
+   ~LcdWriter(){};
+
+   void SetX(Position p) {myModel.SetX(p);};
+   void SetY(Position p) {myModel.SetY(p);};
+   void SetSpindleSpeed(Speed s) {myModel.SetSpindleSpeed(s);};
+   void SetActiveAxis(Axis a) {myModel.SetActiveAxis(a);};
+   void SetAxisView()
+   {
+      delete myView;
+
+      myView = new AxisView<Lcd>(myLcd, myModel);
+      myView->Draw();
+   };
+   void SetSpindleView()
+   {
+      delete myView;
+
+      myView = new SpindleView<Lcd>(myLcd, myModel);
+      myView->Draw();
+   };
+
+   Model myModel;
+
+private:
+   Lcd& myLcd;
+   LcdView<Lcd>* myView;
+   
+};
 
 #endif // __C_LCD_WRITER_H__
--- serialEventHdlr/EventProxy/tests/lcd_writer_tests.cpp
+++ serialEventHdlr/EventProxy/tests/lcd_writer_tests.cpp
@@ -3,6 +3,9 @@
 #include "lcd_writer.h"
 #include <Arduino.h>
 
+using ::testing::_;
+using ::testing::NiceMock;
+
 class MockLcd
 {
    public:
@@ -34,35 +37,145 @@ class MockLcd
    MOCK_METHOD(void, home, (), ());
    MOCK_METHOD(void, print, (const String& str), ());
    MOCK_METHOD(void, setCursor, (uint8_t col, uint8_t row), ());
+   MOCK_METHOD(void, setBacklight, (uint8_t brightness), ());
 };
 
-TEST(LcdWriterTestGroup, clearing)
+/*
+TEST(LcdWriterTestGroup, init)
 {
+   Model lcdModel;
    MockLcd mock_lcd(0x27);
-   LcdWriter<MockLcd> lcdWriter(mock_lcd, String("apa"));
+   EXPECT_CALL(mock_lcd, begin(16, 2));
+   EXPECT_CALL(mock_lcd, setBacklight(200));
+
+   LcdView<MockLcd> lcdView(mock_lcd, lcdModel);
+}*/
+
+TEST(LcdWriterTestGroup, updating)
+{
+   Model lcdModel;
+   NiceMock<MockLcd> mock_lcd(0x27);
+   LcdView<MockLcd> lcdView(mock_lcd, lcdModel);
 
    EXPECT_CALL(mock_lcd, clear());
-   //lcdWriter.clear();
-   lcdWriter();
+   EXPECT_CALL(mock_lcd, setCursor(_, _));
+   EXPECT_CALL(mock_lcd, print(_));
+   lcdView.Draw();
 }
 
-TEST(LcdWriterTestGroup, homing)
+TEST(LcdWriterTestGroup, observer_update)
 {
-   MockLcd mock_lcd(0x27);
-   LcdWriter<MockLcd> lcdWriter(mock_lcd, String("apa"));
+   Model lcdModel;
+   NiceMock<MockLcd> mock_lcd(0x27);
+   LcdView<MockLcd> lcdView(mock_lcd, lcdModel);
 
-   EXPECT_CALL(mock_lcd, home());
-   //lcdWriter.home();
-   lcdWriter();
+   EXPECT_CALL(mock_lcd, clear());
+   EXPECT_CALL(mock_lcd, setCursor(_, _));
+   EXPECT_CALL(mock_lcd, print(_));
+   lcdModel.SetX(1.1f);
 }
 
-TEST(LcdWriterTestGroup, print_simple)
+TEST(LcdWriterTestGroup, axis_view_set_x)
 {
-   String temp_str = String("hej");
-   MockLcd mock_lcd(0x27);
-   LcdWriter<MockLcd> lcdWriter(mock_lcd, String("apa"));
+   Model lcdModel;
+   NiceMock<MockLcd> mock_lcd(0x27);
+   AxisView<MockLcd> axisView(mock_lcd, lcdModel);
 
-   EXPECT_CALL(mock_lcd, print(String("hej")));
-   //lcdWriter.print(String("hej"));
-   lcdWriter(temp_str);
+   EXPECT_CALL(mock_lcd, setCursor(0, 0));
+   EXPECT_CALL(mock_lcd, setCursor(0, 1));
+   EXPECT_CALL(mock_lcd, print(String("*x: 1.1")));
+   EXPECT_CALL(mock_lcd, print(String(" y: 0")));
+   lcdModel.SetX(1.1f);
+}
+
+TEST(LcdWriterTestGroup, axis_view_set_y)
+{
+   Model lcdModel;
+   NiceMock<MockLcd> mock_lcd(0x27);
+   AxisView<MockLcd> axisView(mock_lcd, lcdModel);
+
+   EXPECT_CALL(mock_lcd, setCursor(0, 0));
+   EXPECT_CALL(mock_lcd, setCursor(0, 1));
+   EXPECT_CALL(mock_lcd, print(String("*x: 0")));
+   EXPECT_CALL(mock_lcd, print(String(" y: 99.99")));
+   lcdModel.SetY(99.99f);
+}
+
+TEST(LcdWriterTestGroup, axis_view_set_active_axis)
+{
+   Model lcdModel;
+   NiceMock<MockLcd> mock_lcd(0x27);
+   AxisView<MockLcd> axisView(mock_lcd, lcdModel);
+
+   EXPECT_CALL(mock_lcd, setCursor(0, 0));
+   EXPECT_CALL(mock_lcd, setCursor(0, 1));
+   EXPECT_CALL(mock_lcd, print(String(" x: 0")));
+   EXPECT_CALL(mock_lcd, print(String("*y: 0")));
+   lcdModel.SetActiveAxis(axis_y);
+}
+
+TEST(LcdWriterTestGroup, spindle_view_set_speed)
+{
+   Model lcdModel;
+   NiceMock<MockLcd> mock_lcd(0x27);
+   SpindleView<MockLcd> spindleView(mock_lcd, lcdModel);
+
+   EXPECT_CALL(mock_lcd, setCursor(0, 0));
+   EXPECT_CALL(mock_lcd, setCursor(0, 1));
+   EXPECT_CALL(mock_lcd, print(String(" spindle speed:")));
+   EXPECT_CALL(mock_lcd, print(String(" 1000 rpm")));
+   lcdModel.SetSpindleSpeed(1000);
+}
+
+TEST(LcdWriterTestGroup, lcd_writer_init)
+{
+   MockLcd mock_lcd(0x27);
+
+   EXPECT_CALL(mock_lcd, begin(16, 2));
+   EXPECT_CALL(mock_lcd, setBacklight(200));
+   LcdWriter<MockLcd> lcdWriter(mock_lcd);
+}
+
+TEST(LcdWriterTestGroup, lcd_writer_set_x)
+{
+   NiceMock<MockLcd> mock_lcd(0x27);
+   LcdWriter<MockLcd> lcdWriter(mock_lcd);
+
+   EXPECT_CALL(mock_lcd, setCursor(0, 0));
+   EXPECT_CALL(mock_lcd, setCursor(0, 1));
+   EXPECT_CALL(mock_lcd, print(String("*x: 10.9")));
+   EXPECT_CALL(mock_lcd, print(String(" y: 0")));
+   lcdWriter.SetX(10.9f);
+}
+
+using ::testing::InSequence;
+
+TEST(LcdWriterTestGroup, lcd_writer_set_spindle_view)
+{
+   NiceMock<MockLcd> mock_lcd(0x27);
+   LcdWriter<MockLcd> lcdWriter(mock_lcd);
+
+   {
+      InSequence seq;
+      EXPECT_CALL(mock_lcd, setCursor(0, 0));
+      EXPECT_CALL(mock_lcd, print(String(" spindle speed:")));
+      EXPECT_CALL(mock_lcd, setCursor(0, 1));
+      EXPECT_CALL(mock_lcd, print(String(" 0 rpm")));
+   }
+   lcdWriter.SetSpindleView();
+}
+
+TEST(LcdWriterTestGroup, lcd_writer_set_axis_view)
+{
+   NiceMock<MockLcd> mock_lcd(0x27);
+   LcdWriter<MockLcd> lcdWriter(mock_lcd);
+
+   {
+      InSequence seq;
+      EXPECT_CALL(mock_lcd, setCursor(0, 0));
+      EXPECT_CALL(mock_lcd, print(String("*x: 0")));
+      EXPECT_CALL(mock_lcd, setCursor(0, 1));
+      EXPECT_CALL(mock_lcd, print(String(" y: 0")));
+   }
+   lcdWriter.SetAxisView();
 }
