a8476a2d80d4c5b9a5ca2c710329c7b340c989d9
johanp1 <johan.p1sson@gmail.com>
2022-06-26 09:23:15 +0200

    integrated with lcd_writer

--- serialEventHdlr/mpg_pendant_lathe/mpg_pendant_lathe.ino
+++ serialEventHdlr/mpg_pendant_lathe/mpg_pendant_lathe.ino
@@ -13,93 +13,138 @@
 constexpr auto cNbrOfEventGenerators = cNbrOfButtons + cNbrOfSelectors;
 constexpr auto cHeartbeatPeriod = 2000; //2000ms
 
-class UpdateXCommandHandler : public CommandHandler
+class UpdateXCommandHandler : public EventFunctor
 {
   public:
-    UpdateXCommandHandler(LcdWriter<LiquidCrystal_PCF8574>& lcdWriter) : CommandHandler{String("x")}, lcdWriter_(lcdWriter) {};
+    UpdateXCommandHandler(Model& model) : EventFunctor{String("x")}, model_(model) {};
 
     void operator()(String& _parsedData)
     {
-      lcdWriter_.SetX(_parsedData.toFloat());
+      model_.SetX(_parsedData.toFloat());
     };
 
   private:
-    LcdWriter<LiquidCrystal_PCF8574>& lcdWriter_;
+    Model& model_;
 };
 
-class UpdateYCommandHandler : public CommandHandler
+class UpdateYCommandHandler : public EventFunctor
 {
   public:
-    UpdateYCommandHandler(LcdWriter<LiquidCrystal_PCF8574>& lcdWriter) : CommandHandler{String("y")}, lcdWriter_(lcdWriter) {};
+    UpdateYCommandHandler(Model& model) : EventFunctor{String("y")}, model_(model) {};
 
     void operator()(String& _parsedData)
     {
-      lcdWriter_.SetY(_parsedData.toFloat());
+      model_.SetY(_parsedData.toFloat());
     };
 
   private:
-    LcdWriter<LiquidCrystal_PCF8574>& lcdWriter_;
+    Model& model_;
 };
 
-class UpdateSpindleSpeedCommandHandler : public CommandHandler
+class UpdateSpindleSpeedCommandHandler : public EventFunctor
 {
   public:
-    UpdateSpindleSpeedCommandHandler(LcdWriter<LiquidCrystal_PCF8574>& lcdWriter) : CommandHandler{String("ss")}, lcdWriter_(lcdWriter) {};
+    UpdateSpindleSpeedCommandHandler(Model& model) : EventFunctor{String("ss")}, model_(model) {};
 
     void operator()(String& _parsedData)
     {
-      lcdWriter_.SetSpindleSpeed(_parsedData.toFloat());
+      model_.SetSpindleSpeed(_parsedData.toFloat());
     };
 
   private:
-    LcdWriter<LiquidCrystal_PCF8574>& lcdWriter_;
+    Model& model_;
 };
 
-class UpdateViewCommandHandler : public CommandHandler
+class AxisViewCommandHandler : public EventFunctor
 {
   public:
-    UpdateViewCommandHandler(LcdWriter<LiquidCrystal_PCF8574>& lcdWriter) : CommandHandler{String("sela")}, lcdWriter_(lcdWriter) {};
+    AxisViewCommandHandler(LcdView<LiquidCrystal_PCF8574>& view) : EventFunctor{String("sela")}, view_(view) {};
+
+    void operator()(String& _parsedData)
+    {
+      if((_parsedData.toInt() == 1) || (_parsedData.toInt() == 2))
+      {
+        view_.SetEnabled(true);
+      }
+      else
+      {
+        view_.SetEnabled(false);
+      }
+    };
+
+  private:
+    LcdView<LiquidCrystal_PCF8574>& view_;
+};
+
+class SpindleViewCommandHandler : public EventFunctor
+{
+  public:
+    SpindleViewCommandHandler(LcdView<LiquidCrystal_PCF8574>& view) : EventFunctor{String("sela")}, view_(view) {};
+
+    void operator()(String& _parsedData)
+    {
+      if(_parsedData.toInt() == 3)
+      {
+        view_.SetEnabled(true);
+      }
+      else
+      {
+        view_.SetEnabled(false);
+      }
+    };
+
+  private:
+    LcdView<LiquidCrystal_PCF8574>& view_;
+};
+
+class ActiveAxisCommandHandler : public EventFunctor
+{
+  public:
+    ActiveAxisCommandHandler(Model& model) : EventFunctor{String("sela")}, model_(model) {};
 
     void operator()(String& _parsedData)
     {
       if(_parsedData.toInt() == 1)
       {
-        lcdWriter_.SetActiveAxis(axis_x);
-        lcdWriter_.SetAxisView();
+        model_.SetActiveAxis(axis_x);
       }
       if(_parsedData.toInt() == 2)
       {
-        lcdWriter_.SetActiveAxis(axis_y);
-        lcdWriter_.SetAxisView();
-      }
-      if(_parsedData.toInt() == 3)
-      {
-        lcdWriter_.SetSpindleView();
+        model_.SetActiveAxis(axis_y);
       }
     };
 
   private:
-    LcdWriter<LiquidCrystal_PCF8574>& lcdWriter_;
+    Model& model_;
 };
 
 static LiquidCrystal_PCF8574 lcd(0x27); // set the LCD address to 0x27
-LcdWriter<LiquidCrystal_PCF8574> lcdWriter(lcd);
-//Model lcdModel;
-//AxisView<LiquidCrystal_PCF8574>* axisView;
+Model lcdModel;
 static Sender sender;
 static EventGenerator* eventGenerators[cNbrOfEventGenerators];
 static Receiver receiver(String("rec"));
-static EventParser ep;
-static UpdateXCommandHandler updateXCommandHandler(lcdWriter);
-static UpdateYCommandHandler updateYCommandHandler(lcdWriter);
-static UpdateSpindleSpeedCommandHandler updateSpindleCommandHandler(lcdWriter);
-static UpdateViewCommandHandler updateViewCommandHandler(lcdWriter);
+
 static unsigned long heartbeatTimer = cHeartbeatPeriod;
 
 void setup() {
+  AxisView<LiquidCrystal_PCF8574>* axisView = new AxisView<LiquidCrystal_PCF8574>(lcd, lcdModel);
+  SpindleView<LiquidCrystal_PCF8574>* spindleView =new SpindleView<LiquidCrystal_PCF8574>(lcd, lcdModel);
+
+  UpdateXCommandHandler* updateXCommandHandler = new UpdateXCommandHandler(lcdModel);
+  UpdateYCommandHandler* updateYCommandHandler = new UpdateYCommandHandler(lcdModel);
+  UpdateSpindleSpeedCommandHandler* updateSpindleCommandHandler = new UpdateSpindleSpeedCommandHandler(lcdModel);
+  AxisViewCommandHandler* axisViewCommandHandler = new AxisViewCommandHandler(*axisView);
+  SpindleViewCommandHandler* spindleViewCommandHandler = new SpindleViewCommandHandler(*spindleView);
+  ActiveAxisCommandHandler* activeAxisCommandHandler = new ActiveAxisCommandHandler(lcdModel);
+  
   Serial.begin(38400);
   Serial.setTimeout(500);
   Serial.println("mpgPendant::setup()");
+
+  // initialize the lcd
+  lcd.begin(16, 2); 
+  lcd.clear();
+  lcd.setBacklight(200);
 
   eventGenerators[0] = new Button("func", cFuncButtonPin, cButtonDebounceDelay);
   eventGenerators[0]->addEventListner(&sender);
@@ -115,24 +160,26 @@ void setup() {
 
   eventGenerators[4] = new Selector2("sela", cAxisSelectorPin1, cAxisSelectorPin2, cAxisSelectorPin3, cSelectorDebounceDelay);
   eventGenerators[4]->addEventListner(&sender);
-  eventGenerators[4]->addEventListner(&ep);
 
+  // register selector changes to the view's event handlers
+  eventGenerators[4]->addEventListner(axisView->GetController());
+  eventGenerators[4]->addEventListner(spindleView->GetController());
+  
   //  eventGenerators[5] = new Selector("sels", cScaleSelectorPin, cSelectorDebounceDelay);
   //  eventGenerators[5]->addEventListner(&sender);
 
-  receiver.addEventListner(&ep);
-  ep.AddAcceptedCmd(updateXCommandHandler);
-  ep.AddAcceptedCmd(updateYCommandHandler);
-  ep.AddAcceptedCmd(updateSpindleCommandHandler);
-  ep.AddAcceptedCmd(updateViewCommandHandler);
+  // register serial events the view's event handlers
+  receiver.addEventListner(axisView->GetController());
+  receiver.addEventListner(spindleView->GetController());
 
-    lcd.begin(16, 2); // initialize the lcd
-    lcd.clear();
-    lcd.setBacklight(200);
+  axisView->GetController()->AddAcceptedHandler(*updateXCommandHandler);
+  axisView->GetController()->AddAcceptedHandler(*updateYCommandHandler);
+  spindleView->GetController()->AddAcceptedHandler(*updateSpindleCommandHandler);
 
-    //axisView = new AxisView<LiquidCrystal_PCF8574>(lcd, lcdModel);
-    //axisView->Draw();
-    lcdWriter.SetAxisView();
+  axisView->GetController()->AddAcceptedHandler(*axisViewCommandHandler);
+  spindleView->GetController()->AddAcceptedHandler(*spindleViewCommandHandler);
+
+  axisView->GetController()->AddAcceptedHandler(*activeAxisCommandHandler);
 }
 
 void loop() {
